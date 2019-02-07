53f1b2b26804a738812e78b26f3de16600f08a89
japrine <jprine01@gmail.com>
2019-02-06 23:33:22 -0600

    First

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class Product(db.Model):
     price = db.Column(db.Float)
     qty = db.Column(db.Integer)
 
-    def __init(self, name, description, price, qty):
+    def __init__(self, name, description, price, qty):
         self.name = name
         self.description = description
         self.price = price
@@ -58,6 +58,50 @@ def add_product():
     db.session.commit()
 
     return product_schema.jsonify(new_product)
+
+
+# Get all Products
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result.data)
+
+
+# Get single product
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+
+# Update Product
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.qty = qty
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+
+# Delete Product
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
 
 
 # Run Server
